febdc4c35e3e4b3206cbef2787ca5c6af68c4dd5
Nowell Zyx Sun <nz.sun03@gmail.com>
2017-06-04 12:11:44 +0800

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# captcha-maker
+# Captcha-maker
+*a simple online captcha maker inspired by [the-art-of-web](http://www.the-art-of-web.com/php/captcha/)*
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<title></title>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.2.min.js"></script>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.2.min.js"></script>
-
 </head>
 <body>
 	<p><img id="captchaImg" src="" width="120" height="30" border="1" alt="CAPTCHA"></p>
@@ -18,4 +17,5 @@
 		});
 	});
 </script>
+
 </html>
